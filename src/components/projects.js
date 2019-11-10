a802f34db862a103b4eafe040878db0fe8cf2e08
crgmonroe <crgmonroe@gmail.com>
2019-11-09 20:47:46 -0500

    Added cards for different projects. In-process

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -11,30 +11,78 @@ class Projects extends Component {
         switch (this.state.activeTab) {
             case 0:
                 return (
-                    <div>
-                        <h1>React</h1>
-                    </div>
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                        <CardTitle style={{color: '#FFF', height: '176px', background: 'url(https://s15-us2.startpage.com/cgi-bin/serveimage?url=https:%2F%2Fcdn.iconscout.com%2Ficon%2Ffree%2Fpng-256%2Freact-1-282599.png&sp=0ab541e9b56381d27f1779f831989f87) center / cover'}} >React Project</CardTitle>
+                        <CardText>
+                            This React web site
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#FFF'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+
+                    </Card>
                 )
                 break;
             case 1:
                 return (
-                    <div>
-                        <h1>jQuery/Chrome Plugin</h1>
-                    </div>
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                        <CardTitle style={{color: '#FFF', height: '176px', background: 'url(https://s15-us2.startpage.com/cgi-bin/serveimage?url=https:%2F%2Fcdn.iconscout.com%2Ficon%2Ffree%2Fpng-256%2Freact-1-282599.png&sp=0ab541e9b56381d27f1779f831989f87) center / cover'}} >jQuery/Chrome Plugin</CardTitle>
+                        <CardText>
+                            Chrome plugin for filtering Eureka URLS
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#FFF'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+
+                    </Card>
                 )
                 break;
             case 2:
                 return (
-                    <div>
-                        <h1>Java/Spring</h1>
-                    </div>
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                        <CardTitle style={{color: '#FFF', height: '176px', background: 'url(https://s15-us2.startpage.com/cgi-bin/serveimage?url=https:%2F%2Fcdn.iconscout.com%2Ficon%2Ffree%2Fpng-256%2Freact-1-282599.png&sp=0ab541e9b56381d27f1779f831989f87) center / cover'}} >Java/Spring Projects</CardTitle>
+                        <CardText>
+                            Company Projects
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#FFF'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+
+                    </Card>
                 )
                 break;
             case 3:
                 return (
-                    <div>
-                        <h1>Flutter</h1>
-                    </div>
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                        <CardTitle style={{color: '#FFF', height: '176px', background: 'url(https://s15-us2.startpage.com/cgi-bin/serveimage?url=https:%2F%2Fcdn.iconscout.com%2Ficon%2Ffree%2Fpng-256%2Freact-1-282599.png&sp=0ab541e9b56381d27f1779f831989f87) center / cover'}} >Flutter Project</CardTitle>
+                        <CardText>
+                            In-Process...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#FFF'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+
+                    </Card>
                 )
                 break;
         
@@ -54,8 +102,15 @@ class Projects extends Component {
                     <Tab>Flutter</Tab>
                 </Tabs>
                 <section>
-                    <div className="projects-grid">
-                    {this.switchCategories()}
+                    <div>
+                        <Grid>
+                            <Cell col={12}>
+                                <div className="content">
+                                    {this.switchCategories()}
+                                </div>
+                            </Cell>
+                        </Grid>
+                    
                     </div>
                 </section>
             </div>
