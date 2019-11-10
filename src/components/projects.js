d83897d4bf4a905ad2efb5856f0ca2ba59b4d1e6
crgmonroe <crgmonroe@gmail.com>
2019-11-09 19:55:34 -0500

    basic project categories with switch for state

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
@@ -6,22 +6,65 @@ class Projects extends Component {
         super(props);
         this.state = { activeTab: 0 };
     }
-    
-    
+
+    switchCategories() {
+        switch (this.state.activeTab) {
+            case 0:
+                return (
+                    <div>
+                        <h1>React</h1>
+                    </div>
+                )
+                break;
+            case 1:
+                return (
+                    <div>
+                        <h1>jQuery/Chrome Plugin</h1>
+                    </div>
+                )
+                break;
+            case 2:
+                return (
+                    <div>
+                        <h1>Java/Spring</h1>
+                    </div>
+                )
+                break;
+            case 3:
+                return (
+                    <div>
+                        <h1>Flutter</h1>
+                    </div>
+                )
+                break;
+        
+        
+            default:
+                break;
+        }
+    }
+
     render() {
         return (
-            <div className="demo-tabs">
+            <div className="project-tabs">
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>Starks</Tab>
-                    <Tab>Lannisters</Tab>
-                    <Tab>Targaryens</Tab>
+                    <Tab>React</Tab>
+                    <Tab>jQuery/Chrome Plugin</Tab>
+                    <Tab>Java/Spring</Tab>
+                    <Tab>Flutter</Tab>
                 </Tabs>
                 <section>
-                    <div className="content">Content for the tab: {this.state.activeTab}</div>
+                    <div className="projects-grid">
+                    {this.switchCategories()}
+                    </div>
                 </section>
-            </div>    
+            </div>
         );
     }
 }
+
+
+
+
 
 export default Projects;
